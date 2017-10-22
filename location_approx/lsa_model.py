1df170cf762aa9ad392cf7c5e76973c6e0fd7f11
scouts.dion <scouts.dion@stormbenefits.com>
2017-10-22 23:32:21 +0800

    Modify lsa-model to calculate document similarity

--- location_approx/lsa_model.py
+++ location_approx/lsa_model.py
@@ -1,7 +1,15 @@
 from gensim.models.lsimodel import LsiModel
-from gensim import corpora
+from gensim import corpora, similarities
 import gensim.models as models
 import pandas as pd
+import _thread
+import time
+import datetime
+
+def time_counter(start_time,isRunning):
+	while(isRunning):
+		print("%s elapsed" % (str(datetime.timedelta(seconds=time.time() - start_time))),end='\r')
+
 
 df = pd.DataFrame.from_csv('[pp_FULL] yolanda nov 7.csv')
 lemmas_list = [] 
@@ -18,9 +26,31 @@ for lemmas in df['lemmas']:
 dictionary = corpora.Dictionary(lemmas_list)
 dictionary.save('./nov7_corpus.dict')
 
-clean_doc = dictionary.doc2bow(lemmas_list)
-tfidf = models.TfidfModel(lemmas_list)
+clean_doc = [dictionary.doc2bow(text) for text in lemmas_list]
 
-lsi = LsiModel(corpus=tfidf,num_topics=200)
+tfidf = models.TfidfModel(clean_doc, normalize=True)
 
-#print(lsi[corpus[text]])
+isRunning = True
+
+start_time = time.time()
+
+_thread.start_new_thread(time_counter,(start_time,isRunning))
+
+# lsi = LsiModel(corpus=tfidf[clean_doc], id2word=dictionary,num_topics=200)
+# lsi.save('model.txt')
+
+lsi = LsiModel.load('model.txt')
+
+
+index = similarities.MatrixSimilarity(lsi[clean_doc])
+
+corpus = lsi[dictionary.doc2bow(lemmas_list[0])]
+# print(time.time()-start_time)
+with open('./outputs_clean.txt', 'w+') as file:
+	for doc in sorted(enumerate(index[corpuscd ]), key=lambda item: -item[1]):
+		file.write(str(doc) + '\n')
+
+isRunning = False
+time.sleep(1)
+
+print(str(datetime.timedelta(seconds=time.time() - start_time)))
