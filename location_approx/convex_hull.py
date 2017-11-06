Add radius computation and fix midpoint selection

location_approx/convex_hull.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
+import math
 from scipy.spatial import ConvexHull
+from haversine import haversine
 
 df = pd.DataFrame.from_csv('dataset_yolanda.csv')
 lat_list = [] 
@@ -24,7 +26,7 @@ similarities = open('similarities.txt','r')
 points = []
 
 isFirst = True
-counter = 5
+counter = 10
 for line in similarities:
 	if isFirst:
 		isFirst = not isFirst 
@@ -39,6 +41,8 @@ for line in similarities:
 
 points = np.array(points)
 
+print(points)
+print(points[0][0])
 
 hull = ConvexHull(points)
 
@@ -46,7 +50,41 @@ import matplotlib.pyplot as plt
 
 plt.plot(points[:,0], points[:,1], 'o')
 
+cx = np.mean(hull.points[hull.vertices,0])
+cy = np.mean(hull.points[hull.vertices,1])
+
+print(hull.points[hull.vertices])
+
+plt.plot(cx, cy, 'o')
+
 for simplex in hull.simplices:
 	plt.plot(points[simplex, 0], points[simplex, 1], 'k-')
+
+
+
+counter = 1
+midpoints = []
+for point in hull.points[hull.vertices]:
+
+	if counter == len(points):
+		counter = 0
+	px = (point[0] + points[counter][0])/2
+	py = (point[1] + points[counter][1])/2
+	midpoints.append([px,py])
+	counter += 1
+
+print(len(midpoints))
+
+def get_distance(id, point):
+	return [id, math.sqrt(math.pow((cx-point[0]),2) + math.pow(cy-point[1], 2))]
+
+distances = []
+for index in range(len(midpoints)):
+	distances.append(get_distance(index, midpoints[index]))
+
+distances.sort(key=lambda x: x[1])
+
+index = distances[0][0]
+print(haversine((cx,cy),(midpoints[index][0],midpoints[index][1])))
 
 plt.show()