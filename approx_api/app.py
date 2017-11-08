459f68156cb0c50866e2b0470a1545b484d04d09
irenetherese <irene.bermejo@obf.ateneo.edu>
2017-11-08 09:15:52 +0800

    Add route return city, Modify to connect to ebayanihan db

--- approx_api/app.py
+++ approx_api/app.py
@@ -7,18 +7,21 @@ import approx_api
 app = Flask(__name__)
 start = int(round(time.time()))
 
-a = approx_api.ApproximationAPI("localhost", "ebayanihan",  "it",  "5432")
+a = approx_api.ApproximationAPI("localhost", "ebayanihan_development",  "postgres",  "8080")
 a.test()
+a.connect()
 
 @app.route("/")
 def hello_world():
     return "Hello World Flask Test!"
 
-
 @app.route("/get_tweets")
 def get_tweets():
-	a.connect()
 	return a.get_tweet_data(10)
+
+@app.route("/get_city/<float:lat>/<float:lon>")
+def get_place(lat, lon):
+    return a.get_location_name(lat, lon)
 
 if __name__ == '__main__':
     parser = optparse.OptionParser(usage="python3 app.py -p ")
