c925031004a0c2afe05df0bbfcb6414cc53b4742
irenetherese <irene.bermejo@obf.ateneo.edu>
2018-04-04 18:44:42 +0800

    Debug app routes

--- approx_api/app.py
+++ approx_api/app.py
@@ -130,7 +130,7 @@ def get_tweet_vis_data_limit_ph(collection_id, start_row):
 
 @app.route("/create_handler/<int thread_id>")
 def create_handler(thread_id):
-    return os.system('create_handler.py ' + str(id))
+    return os.system('create_handler.py ' + str(thread_id))
 
 @app.route('/get_location',methods=['GET'])
 def approx_location():
